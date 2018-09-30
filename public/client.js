c330af38f9321d2fff974ebf735682ddd4602c6c
codedevil9 <noam.van.der.schroeff@hva.nl>
2018-10-01 01:20:24 +0200

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,3 +1,6 @@
+// This is a simple javascript code that is responsible for the button on the html site and when you press the button
+// it will display "Je sessie token is 'sessiontoken'"
+
 $(function() {
     $('#myButton').click(function() {
         $.get('/token', function(data) {
